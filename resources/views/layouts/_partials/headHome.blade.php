fb8755b791651374fac66a8d23048cd0736a7fcd
Adib Farhan <adib.farhan@dbt.com.my>
2025-11-11 22:51:06 +0800

    fix css

--- resources/views/layouts/_partials/headHome.blade.php
+++ resources/views/layouts/_partials/headHome.blade.php
@@ -11,7 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="{{ asset('assets/vendors/themify-icons/css/themify-icons.css') }}">
-    <link href="https://adib-portfolio-sigma.vercel.app/assets/style.css" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/style.css') }}" rel="stylesheet" type="text/css" />
 
 
     @stack('css')
